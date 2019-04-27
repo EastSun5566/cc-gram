2d2ef0687feb1705e7c4ad3e50eaa64e74af6b2c
汪東陽 <michael19920327@gmail.com>
2019-04-27 23:15:28 +0800

    ✨ 新增取得 DataUrl 方法

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,12 @@ export default class {
   private _filters = filters;
 
   public constructor() {
-    this.applyFilter();
+    if (document.readyState === 'complete') {
+      this.applyFilter();
+      return;
+    }
+
+    document.addEventListener('DOMContentLoaded', this.applyFilter);
   }
 
   public get filterNameList(): string[] {
@@ -31,11 +36,47 @@ export default class {
   }
 
   public applyFilter(): void {
-    const targets: NodeListOf<HTMLElement> = document.querySelectorAll('img[data-filter]');
+    const targetImages: NodeListOf<HTMLImageElement> = document.querySelectorAll('img[data-filter]');
 
-    targets.forEach((target): void => {
-      const filterName = target.dataset.filter;
-      target.style.filter = this.getFilterStyle(filterName);
+    targetImages.forEach((target): void => {
+      const { dataset } = target;
+      target.style.filter = this.getFilterStyle(dataset.filter);
+    });
+  }
+
+  public getDataUrl(imageElement: HTMLImageElement): Promise<string> {
+    if (!imageElement || imageElement.tagName !== 'IMG') throw new Error();
+
+    const {
+      src,
+      dataset,
+    } = imageElement;
+
+    if (!src) return Promise.resolve('');
+
+    return new Promise((resolve, reject): void => {
+      const image = new Image();
+
+      image.onload = ({ target }): void => {
+        const { width, height } = target;
+        const canvas = document.createElement('canvas');
+
+        canvas.width = width;
+        canvas.height = height;
+
+        const ctx = canvas.getContext('2d');
+        if (!ctx) throw new Error();
+
+        ctx.filter = this.getFilterStyle(dataset.filter);
+        ctx.drawImage(target, 0, 0);
+
+        resolve(canvas.toDataURL());
+      };
+
+      image.onerror = (error): void => reject(error);
+
+      image.crossOrigin = 'anonymous';
+      image.src = src;
     });
   }
 }
