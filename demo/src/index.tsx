d0ce05d283580abb5eb2fc2297925df58d58eb94
汪東陽 <michael19920327@gmail.com>
2020-01-02 22:22:40 +0800

    Update demo

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.scss';
+import './index.scss';
 import App from './App/App';
 // import * as serviceWorker from './serviceWorker';
 
