58092ce4a939864966d339a6c97753d39dfc38da
汪東陽 <michael19920327@gmail.com>
2020-04-20 01:48:19 +0800

    refactor(test): refactor test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -3,37 +3,41 @@ import { CCGram } from '../src';
 import { DEFAULT_FILTERS } from '../src/filters';
 
 describe('CCGram class', (): void => {
-  const IMAGE_SRC = 'https://media.giphy.com/media/sIIhZliB2McAo/giphy.gif';
-  const FILTER_NAME = '1977';
-
-  document.body.innerHTML = `
-    <img
-      src="${IMAGE_SRC}"
-      data-filter="${FILTER_NAME}">
-  `;
-
-  const cg = new CCGram();
-
   test('get filter names list', (): void => {
+    const cg = new CCGram();
+
     expect(cg.filterNames).toEqual([...DEFAULT_FILTERS.keys()]);
   });
 
   test('add filter', (): void => {
+    const cg = new CCGram();
     cg.setFilter('my-filter', { saturate: 0.8 });
 
     expect(cg.filterNames).toContain('my-filter');
   });
 
   test('remove filter', (): void => {
+    const cg = new CCGram();
     cg.removeFilter('my-filter');
 
     expect(cg.filterNames.includes('my-filter')).toBe(false);
   });
 
-  const target = document.querySelector<HTMLImageElement>(`img[data-filter="${FILTER_NAME}"]`);
-  if (!target) throw new Error('No target');
-
   test('applyFilter method', (): void => {
+    const IMAGE_SRC = 'https://media.giphy.com/media/sIIhZliB2McAo/giphy.gif';
+    const FILTER_NAME = '1977';
+
+    document.body.innerHTML = `
+      <img
+        src="${IMAGE_SRC}"
+        data-filter="${FILTER_NAME}">
+    `;
+
+    const cg = new CCGram();
+
+    const target = document.querySelector<HTMLImageElement>(`img[data-filter="${FILTER_NAME}"]`);
+    if (!target) throw new Error('No target');
+
     expect(cg.getFilterStyle(FILTER_NAME)).toBe(target.style.filter);
   });
 
