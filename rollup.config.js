f88d0b26eeaa740f666a7f3d229a04f4632d427e
汪東陽 <michael19920327@gmail.com>
2021-01-16 04:08:32 +0800

    chore: use rollup ts plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,8 @@
-import { DEFAULT_EXTENSIONS } from '@babel/core';
 import { nodeResolve, DEFAULTS } from '@rollup/plugin-node-resolve';
-import { babel } from '@rollup/plugin-babel';
+import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 
-import { name, main, module } from './package.json';
-
-/**
- * @param {string} str
- */
-const camalize = (str) => str
-  .toLowerCase()
-  .replace(/[^a-zA-Z0-9]+(.)/g, (_, chr) => chr.toUpperCase());
+import { main, module } from './package.json';
 
 /**
  * @type {import('rollup').RollupOptions}
@@ -19,22 +11,21 @@ const config = {
   input: 'src/index.ts',
   output: [
     {
-      name: camalize(name),
+      name: 'CCGram',
       file: main,
       format: 'umd',
       sourcemap: true,
       exports: 'named',
     },
-    { file: module, format: 'es', sourcemap: true },
+    {
+      file: module,
+      format: 'es',
+      sourcemap: true,
+    },
   ],
   plugins: [
     nodeResolve({ extensions: [...DEFAULTS.extensions, '.ts'] }),
-    babel({
-      extensions: [...DEFAULT_EXTENSIONS, '.ts'],
-      presets: ['@babel/env', '@babel/typescript'],
-      plugins: ['@babel/proposal-class-properties'],
-      babelHelpers: 'bundled',
-    }),
+    typescript(),
     terser(),
   ],
 };
