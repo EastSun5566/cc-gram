98ee28efd55a2f545bf5c538e8ea43f05cfa3855
Michael Wang <michael19920327@gmail.com>
2025-12-29 04:28:46 +0800

    chore: lint fix

--- eslint.config.js
+++ eslint.config.js
@@ -15,15 +15,15 @@ export default [
       'no-param-reassign': ['error', { props: false }],
       'no-nested-ternary': 'off',
       'import-x/extensions': 'off',
-      
+
       // Allow default export with name "default"
       'no-restricted-exports': ['error', {
         restrictedNamedExports: ['then'], // Only restrict 'then' as Airbnb does
       }],
-      
+
       // Allow return values from promise executor
       'no-promise-executor-return': 'off',
-      
+
       // Increase max line length to match existing code
       '@stylistic/max-len': ['error', {
         code: 120,
